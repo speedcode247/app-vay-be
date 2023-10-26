bf29218fd4bef2e97045041a30cd41c3b2ee6fee
chaunnt <chaupad@gmail.com>
2023-10-26 17:46:34 +0700

    them trang thai tu choi

--- src/modules/requests/services.js
+++ src/modules/requests/services.js
@@ -100,6 +100,17 @@ export const updateStatus = async ({ requestId, status, error, amount }) => {
     });
     owner.balance = owner.balance - amount;
     owner.save();
+  } else if (status === 'rejected') {
+    await paymentService.createPayment({
+      payload: {
+        userId: current_request.userId,
+        amount: amount,
+        description: 'Từ chối rút tiền',
+        status: false,
+      },
+    });
+    owner.balance = owner.balance - amount;
+    owner.save();
   }
 
   await Model.findByIdAndUpdate(requestId, { status, error });
